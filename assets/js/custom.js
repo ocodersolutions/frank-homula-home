fdfe6fb5682d8ce6f9847f64b742a7404576b1da
Nguyen Sanh Toan <stoannguyen2205@gmail.com>
2016-11-03 17:39:22 +0700

    upgrade

---
+++ assets/js/custom.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+	screen.width
+	if(screen.width >1199 && screen.width <= 1920)
+	{
+		$(".container").width() = screen.width - 50;
+	}
+	else if(screen.width > 1920) 
+	{
+		$(".container").width() = 1860;
+	}
+});
