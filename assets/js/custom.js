9a95e2dc1b6af736d8c79bcf1a65962824b98853
Nguyen Sanh Toan <stoannguyen2205@gmail.com>
2016-11-04 08:05:45 +0700

    upgrade hot-properties

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,11 +1,3 @@
 $(document).ready(function(){
-	screen.width
-	if(screen.width >1199 && screen.width <= 1920)
-	{
-		$(".container").width() = screen.width - 50;
-	}
-	else if(screen.width > 1920) 
-	{
-		$(".container").width() = 1860;
-	}
+	
 });
